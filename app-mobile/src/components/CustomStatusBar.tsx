4504a0db463437ca1703528a3c01e1082675fe2c
andre.souza <andre.souza@programmersinc.com>
2022-11-03 15:41:01 -0300

    setup app mobile

---
+++ app-mobile/src/components/CustomStatusBar.tsx
@@ -0,0 +1,11 @@
+import { StatusBar } from "native-base";
+
+export const CustomStatusBar = () => {
+  return (
+    <StatusBar
+      backgroundColor="transparent"
+      barStyle="light-content"
+      translucent
+    />
+  );
+};
---
+++ app-mobile/src/components/index.ts
@@ -0,0 +1,4 @@
+import { Loading } from "./Loading";
+import { CustomStatusBar } from "./CustomStatusBar";
+
+export { Loading, CustomStatusBar };
